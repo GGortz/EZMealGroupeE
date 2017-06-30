990aefae5f3b7e31d48e4cb9c9bb2632611815d0
GGortz <gaetan.gortz@student.uclouvain.be>
2017-06-30 09:50:44 +0200

    Initial commit

---
+++ app/src/main/java/be/lsinf1225/ezmealgroupee/Acceuil.java
@@ -0,0 +1,13 @@
+package be.lsinf1225.ezmealgroupee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Acceuil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil);
+    }
+}
