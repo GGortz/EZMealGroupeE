5052dbe5222a9e13e1e7c9b314184fd438dff58f
GGortz <gaetan.gortz@student.uclouvain.be>
2017-06-30 12:06:16 +0200

    inscriptionok

--- app/src/main/java/be/lsinf1225/ezmealgroupee/Inscription.java
+++ app/src/main/java/be/lsinf1225/ezmealgroupee/Inscription.java
@@ -1,11 +1,17 @@
 package be.lsinf1225.ezmealgroupee;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Gaetan on 30/06/2017.
@@ -13,27 +19,59 @@ import android.widget.Spinner;
 
 public class Inscription extends AppCompatActivity{
     Spinner sexe;
-    ArrayAdapter<CharSequence> adapSex;
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inscription);
+    }
 
-        sexe=(Spinner) findViewById(R.id.SpinSex);
-        adapSex = ArrayAdapter.createFromResource(this,R.array.SpinSex,android.R.layout.simple_spinner_item);
-        adapSex.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sexe.setAdapter(adapSex);
-        sexe.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    public void onBtnClick(View view){
+        if(view.getId()==R.id.inscriptionEnregistrer){   //Quand on clique sur le bouton enregistrer
+            TextView login=(EditText)findViewById(R.id.txtLogin);
+            TextView mDP=(EditText)findViewById(R.id.txtMDP);
+            TextView mDP2=(EditText)findViewById(R.id.txtMDP2);
+            //spinsexe
+            TextView jour=(EditText)findViewById(R.id.txtJour);
+            TextView mois=(EditText)findViewById(R.id.txtMois);
+            TextView annee=(EditText)findViewById(R.id.txtAnnee);
+            TextView ville=(EditText)findViewById(R.id.txtVille);
+            TextView pays=(EditText)findViewById(R.id.txtPays);
 
+            String uLogin=login.getText().toString();
+            String uMDP=mDP.getText().toString();
+            String uMDP2=mDP2.getText().toString();
+            String uSexe;
+            if(sexe.getSelectedItem().toString().equals("Homme"))  uSexe="Homme";
+            else  uSexe="Femme";
+            int uJour=Integer.parseInt(jour.getText().toString());
+            int uMois=Integer.parseInt(mois.getText().toString());
+            int uAnnee=Integer.parseInt(annee.getText().toString());
+            String uVille=ville.getText().toString();
+            String uPays=pays.getText().toString();
+
+            if(uMDP.equals(uMDP2)){  //Si les MDP sont égaux, ont crée un utilisateur
+                objetUtilisateur u = new objetUtilisateur();
+                u.setLogin(uLogin);
+                u.setMDP(uMDP);
+                u.setSexe(uSexe);
+                u.setJour(uJour);
+                u.setMois(uMois);
+                u.setAnnee(uAnnee);
+                u.setVille(uVille);
+                u.setPays(uPays);
+                //Ajout dans la DB
+                Intent retouracceuil=new Intent(Inscription.this, Acceuil.class);
+                startActivity(retouracceuil);
+            }
+            else{ //Si les MDP ne sont pas égaux, on le signale à l'utilisateur
+                Toast mdpnonegaux = Toast.makeText(Inscription.this, "Les mot de passe ne sont pas égaux, veuillez vérifier.",Toast.LENGTH_LONG);
+                mdpnonegaux.show();
             }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+        }
 
-            }
-        });
+
+
     }
 }
--- app/src/main/java/be/lsinf1225/ezmealgroupee/objetUtilisateur.java
+++ app/src/main/java/be/lsinf1225/ezmealgroupee/objetUtilisateur.java
@@ -5,4 +5,93 @@ package be.lsinf1225.ezmealgroupee;
  */
 
 public class objetUtilisateur {
+
+    private String login;
+    private String mdp;
+    private String sexe;
+    private int jour;
+    private int mois;
+    private int annee;
+    private String ville;
+    private String pays;
+
+
+    public objetUtilisateur(String login, String mdp, String sexe, int jour, int mois, int annee, String ville, String pays) {
+        this.login = login;
+        this.mdp = mdp;
+        this.sexe = sexe;
+        this.jour = jour;
+        this.mois = mois;
+        this.annee = annee;
+        this.ville = ville;
+        this.pays = pays;
+    }
+
+    public objetUtilisateur(){
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getMDP() {
+        return mdp;
+    }
+
+    public void setMDP(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public int getJour() {
+        return jour;
+    }
+
+    public void setJour(int jour) {
+        this.jour = jour;
+    }
+
+    public int getMois() {
+        return mois;
+    }
+
+    public void setMois(int mois) {
+        this.mois = mois;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
 }
